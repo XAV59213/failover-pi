d43386c6330548958700327f3cf5f607de3a4b60
XAV59213 <153436936+XAV59213@users.noreply.github.com>
2025-11-13 16:23:03 +0100

    Update connect_4g.sh

--- home/xavier/connect_4g.sh
+++ home/xavier/connect_4g.sh
@@ -5,12 +5,17 @@ DEVICE="/dev/cdc-wdm0"
 
 APN=$(jq -r '.apn' "$CONFIG_FILE")
 
+# Mettre le modem en mode online
 qmicli -d $DEVICE --dms-set-operating-mode=online
 
+# Allocation du client ID
 CID=$(qmicli -d $DEVICE --wds-allocate-cid | grep 'CID' | awk '{print $2}' | tr -d "'")
 
+# Démarrage connexion data
 qmicli -d $DEVICE --wds-start-network=apn="$APN" --client-cid=$CID --client-no-release-cid
 
+# Attribution IP au modem
 dhclient wwan0
 
+# Affichage
 ip addr show wwan0
