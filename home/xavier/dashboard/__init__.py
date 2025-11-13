d8e792a2c09adb91368e5c1656a79ca433af19b7
XAV59213 <153436936+XAV59213@users.noreply.github.com>
2025-11-13 16:27:05 +0100

    Update __init__.py

--- home/xavier/dashboard/__init__.py
+++ home/xavier/dashboard/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 
+# Création instance Flask
 app = Flask(__name__)
 
+# Chargement config
 from .config import set_app_config
 set_app_config(app)
 
+# Authentification / gardes
 from .auth import register_auth_guards
 register_auth_guards(app)
 
+# Routes principales
 from .routes import register_routes
 register_routes(app)
