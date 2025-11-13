dbe880456c3604380a3e661cad64eca0bb026042
XAV59213 <153436936+XAV59213@users.noreply.github.com>
2025-11-13 18:47:37 +0100

    Create main.js

---
+++ home/xavier/dashboard/static/js/main.js
@@ -0,0 +1,14 @@
+/* ==========================================================
+   Failover-Pi Dashboard - JS
+   ========================================================== */
+
+console.log("Failover-Pi Dashboard static JS loaded ✔️");
+
+/* Auto-refresh option (disabled by default)
+function autoRefresh() {
+    setTimeout(() => {
+        location.reload();
+    }, 10000);
+}
+autoRefresh();
+*/
